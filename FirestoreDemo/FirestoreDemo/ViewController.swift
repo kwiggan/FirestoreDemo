42de6c23f054ed648a0d3c2d6b2c9fc8e8783970
Keneisha Wiggan <Keneishawiggan8@gmail.com>
2019-09-15 11:34:42 -0400

    Set up firebase and coco pod to implement user Authentication

---
+++ FirestoreDemo/FirestoreDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirestoreDemo
+//
+//  Created by Keneisha Wiggan on 9/15/19.
+//  Copyright © 2019 Keneisha Wiggan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
